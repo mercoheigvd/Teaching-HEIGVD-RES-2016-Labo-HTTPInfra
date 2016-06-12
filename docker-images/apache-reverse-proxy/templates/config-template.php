11de320969f51db613b216f135db6f762c3e1527
mercodrums <romain.mercier@hotmail.ch>
2016-06-12 13:06:13 +0200

    Configuration dymanique ok

---
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -0,0 +1,13 @@
+<?php
+	$static_app = getenv('STATIC_APP');
+	$dynamic_app = getenv('DYNAMIC_APP');
+?>
+<VirtualHost *:80>
+	ServerName demo.res.ch
+	
+	ProxyPass '/api/color/' 'http://<?php print "$dynamic_app"; ?>/'
+	ProxyPassReverse '/api/color/' 'http://<?php print "$dynamic_app"; ?>/'
+	
+	ProxyPass '/' 'http://<?php print "$static_app"; ?>/'
+	ProxyPassReverse '/' 'http://<?php print "$static_app"; ?>/'
+</VirtualHost>
